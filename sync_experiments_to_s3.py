7fdd710f2570cdf2dee35a410b171f154ab799e1
Miles Crabill <mcrabill@mozilla.com>
2016-01-07 16:47:23 -0800

    update with format changes

--- sync_experiments_to_s3.py
+++ sync_experiments_to_s3.py
@@ -3,29 +3,29 @@ import sys, yaml, json, boto.s3.connection, boto.s3.key
 
 def main():
   if len(sys.argv) != 2:
-    sys.exit("%s accepts one argument, path to a YAML configuration file" % sys.argv[0])
+    sys.exit("{0} accepts one argument, path to a YAML configuration file".format(sys.argv[0]))
   config_filename = sys.argv[1]
 
   # open config
   try:
     config_file = open(config_filename, 'rb')
   except IOError:
-    sys.exit("Could not open configuration file: %s" % config_filename)
+    sys.exit("Could not open configuration file: {0}".format(config_filename))
 
   with config_file:
     # parse config
     try:
       y = yaml.load(config_file.read())
     except yaml.YAMLError:
-      sys.exit("Error in configuration file: %s" % config_filename)
+      sys.exit("Error in configuration file: {0}".format(config_filename))
 
     # check for value existence
     if not y.get("experiments_filename"):
-      sys.exit("No value for 'experiments_filename' in config %s" % config_filename)
+      sys.exit("No value for 'experiments_filename' in config {0}".format(config_filename))
     if not y.get("bucket"):
-      sys.exit("No value for 'bucket' in config %s" % config_filename)
+      sys.exit("No value for 'bucket' in config {0}".format(config_filename))
     if not y.get("upload"):
-      sys.exit("No value for 'upload' in config %s" % config_filename)
+      sys.exit("No value for 'upload' in config {0}".format(config_filename))
 
     # done with config file after parsing
     config_file.close()
@@ -40,20 +40,20 @@ def main():
     try:
       experiments_file = open(experiments_filename, 'rb')
     except IOError:
-      sys.exit("Could not read experiments file: %s" % experiments_filename)
+      sys.exit("Could not read experiments file: {0}".format(experiments_filename))
 
     with experiments_file:
       # save experiments text in string
       experiments = experiments_file.read()
       experiments_file.close()
 
-      print "*** %s file contents ***\n%s" % experiments_filename, experiments
+      print "*** {0} file contents ***\n{1}".format(experiments_filename, experiments)
 
       # check for valid json
       try:
         j = json.loads(experiments)
       except ValueError:
-        sys.exit("Invalid JSON in experiments file: %s" % experiments_filename)
+        sys.exit("Invalid JSON in experiments file: {0}".format(experiments_filename))
 
       # initialize a boto
       conn = boto.s3.connection.S3Connection()
@@ -65,7 +65,7 @@ def main():
       k.set_contents_from_string(experiments)
 
       # success
-      print "Uploaded %s to s3" % experiments_filename
+      print "Uploaded {0} to s3".format(experiments_filename)
 
 if __name__ == "__main__":
   main()
